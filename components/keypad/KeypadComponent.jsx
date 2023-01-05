8236553747e6d13aef0635caf4aad3e3ec60c8d9
Aleksandre Tcharbadze <119774487+LynxVault@users.noreply.github.com>
2023-01-04 22:26:57 -0500

    re-aligning the keypad buttons

--- components/keypad/KeypadComponent.jsx
+++ components/keypad/KeypadComponent.jsx
@@ -159,7 +159,7 @@ const KeypadComponent = (props) => {
                 <div className="flex w-[100%] h-[7.5rem]">
                     <div className="m-auto space-x-4">
                         <button className=" border-2 w-[6rem] h-[6rem]
-                                            translate-y-[-.15rem] 
+                                            translate-y-[-.16rem] 
                                             rounded-[20px] text-4xl
                                             hover:bg-white
                                             hover:text-black"
@@ -168,7 +168,7 @@ const KeypadComponent = (props) => {
                                                 C
                         </button>
                         <button className=" border-2 w-[6rem] h-[6rem] 
-                                            translate-y-[-.15rem]
+                                            translate-y-[-.2rem]
                                             rounded-[20px] text-4xl
                                             hover:bg-white
                                             hover:text-black"
